b09a4c21c42853915b3e0a1d52d5a9843d1141cb
ivanshamis <ivanshamis@gmail.com>
2018-07-15 15:49:28 +0300

    task1

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::put('/api/currencies/{id}/rate','ApiController@put')->middleware('put.rate');
